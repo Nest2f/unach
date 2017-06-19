9afda09a6ba76ea5abf2e8a76993b503d1185e60
Nest2f <angelo_jmg_52@hotmail.com>
2017-06-19 12:48:36 -0500

    ccf

--- controlador/UploadFiles.php
+++ controlador/UploadFiles.php
@@ -3,8 +3,7 @@ require_once  '../controlador/controladorDocumento.php';
 require_once '../modelo/claseDocumento.php';
 require_once '../modelo/connect.php';
 
-$upload_files = './documentos/';
-
+$upload_files = 'documentos/';
 $nombre_archivo = $_FILES['archivo']['name'];
 $tipo_archivo = $_FILES['archivo']['type'];
 $tmp_archivo = $_FILES['archivo']['tmp_name'];
@@ -12,11 +11,13 @@ $archivador = $upload_files . '/' . $nombre_archivo;
 $link_archivo;
 if (move_uploaded_file($tmp_archivo, $archivador)) {
 
-    $link_archivo = $hostarchivo_formacion_academica . $nombre_archivo;
+    $link_archivo = $upload_files . $nombre_archivo;
 } else {
     $link_archivo = "";
 }
-$nume=isset($_POST['nume_documento'])?$_POST['nume_documento']:null;
+
 $archivo=$link_archivo;
-$modi=new controladorDocumento();
-$modi->actualizarDocumento($nume, $archivo);
+$nume_documento = isset($_POST['Nume_Documento']) ? $_POST['Nume_Documento'] : null;
+$actualizar = new controladorDocumento;
+$aarch = $actualizar->actualizarDocumento($nume_documento, $archivo);
+
