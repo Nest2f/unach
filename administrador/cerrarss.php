901147e0c1b0d315e8350ff0a8a4c03c2bf7e2ab
Nest2f <angelo_jmg_52@hotmail.com>
2017-07-09 23:46:36 -0500

    cuak

--- administrador/cerrarss.php
+++ administrador/cerrarss.php
@@ -1,7 +1,10 @@
 <?php
 
 session_start();
-setcookie('nueva', $_SESSION['Id_Tipo_Usuario'], time() - 18000);
+if (isset($_COOKIE['nueva'])) {
+    unset($_COOKIE['nueva']);
+    setcookie('nueva', '', time() - 18000, '/');
+}
 session_destroy();
 echo "<script> alert('Sesión Finalizada');</script>";
 header("refresh:0;url=../index.php");
